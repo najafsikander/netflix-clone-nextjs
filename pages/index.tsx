30ad85b8e6feda3deba715f566b5c58e6ed53585
najafsikander <najafsikander@yahoo.com>
2023-10-03 17:02:50 +0400

    completed billboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
 import { getSession } from "next-auth/react";
 import { NextPageContext } from "next";
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
-  if(!session) return {redirect: {destination: '/auth', permanent: false}}
+  if (!session) return { redirect: { destination: "/auth", permanent: false } };
 
   return {
-    props: {}
-  }
+    props: {},
+  };
 }
 
 export default function Home() {
-
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <Billboard />
     </>
-  )
+  );
 }
-
